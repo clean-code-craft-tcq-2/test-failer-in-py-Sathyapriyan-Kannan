4eec15161678872d5a3f3d70e11671e0aba00519
Sathyapriyan Kannan <sathyapriyankannan@gmail.com>
2022-01-27 17:41:10 +0530

    Removed Unwanted Comments Altered Stub in alerter.py and implemented SRP

--- alerter.py
+++ alerter.py
@@ -2,15 +2,19 @@ alert_failure_count = 0
 
 
 def network_alert_stub(celcius):
+    threshold = 175
     print(f'ALERT: Temperature is {celcius} celcius')
     # Return 200 for ok
     # Return 500 for not-ok
     # stub always succeeds and returns 200
-    return 200
+    if celcius <= threshold:
+        return 200
+    else:
+        return 500
 
 
-def alert_in_celcius(farenheit):
-    celcius = (farenheit - 32) * 5 / 9
+def alert_in_celcius(fahrenheit):
+    celcius = convert_fahrenheit_to_celcius(fahrenheit)
     returnCode = network_alert_stub(celcius)
     if returnCode != 200:
         # non-ok response is not an error! Issues happen in life!
@@ -21,8 +25,13 @@ def alert_in_celcius(farenheit):
         alert_failure_count += 0
 
 
+def convert_fahrenheit_to_celcius(fahrenheit):
+    celcius = (fahrenheit - 32) * 5 / 9
+    return celcius
+
+
 alert_in_celcius(400.5)
 alert_in_celcius(303.6)
-assert(alert_failure_count == 2)
+assert(alert_failure_count == 1)
 print(f'{alert_failure_count} alerts failed.')
 print('All is well (maybe!)')
--- misaligned.py
+++ misaligned.py
@@ -5,14 +5,12 @@ def print_color_map():
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
             color_map.append(f'{i * 5 + j} | {major} | {minor}')
-            # print(f'{i * 5 + j} | {major} | {minor}')
 
     return len(major_colors) * len(minor_colors), color_map
 
 
 result, color_map = print_color_map()
 
-# assert(result == 25)
 assert (color_map[0] == '1 | White | Blue')
 assert (color_map[2] == '3 | White | Brown')
 assert (color_map[15] == '16 | Yellow | Orange')
